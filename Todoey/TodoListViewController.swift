e24a0500e6de3d25d9f7687527e8f417b25b5efd
AlexNov12 <nov.alex96@gmail.com>
2024-02-07 06:08:49 +0300

    "Add dataSource func's to controller"

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,6 +17,16 @@ class TodoListViewController: UITableViewController {
     }
 
     // Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
 }
 
