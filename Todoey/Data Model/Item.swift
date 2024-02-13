080c950d873b3d4d752030b1d911104d7fa63bb2
AlexNov12 <nov.alex96@gmail.com>
2024-02-13 06:14:42 +0300

    Created MVC struct. Added plist(codable)

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Александр Новиков on 11.02.2024.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
+
