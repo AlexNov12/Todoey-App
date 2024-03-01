189b9a5423dd5dcfd71b1c63e903728cde55cf7e
AlexNov12 <nov.alex96@gmail.com>
2024-03-01 06:13:25 +0300

    Added SwipeTableVC

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 import RealmSwift
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
     
     var todoItems : Results<Item>?
     var realm = try! Realm()
@@ -34,7 +34,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
@@ -95,20 +95,22 @@ class TodoListViewController: UITableViewController {
     
     //MARK - Model Manipulation Methods
     
-//    func saveItems() {
-//        do {
-//           try context.save()
-//        } catch {
-//            print("Error saving context \(error)")
-//        }
-//        self.tableView.reloadData() // Чтобы обновились данные в массиве в общем скоупе
-//    }
-//    
     func loadItems() {
         
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         
         tableView.reloadData()
+    }
+    override func updateModel(at indexPath: IndexPath) {
+        if let itemForDeletion = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    realm.delete(itemForDeletion)
+                }
+            } catch{
+                print("Error deleting item, \(error)")
+            }
+        }
     }
 }
 
@@ -128,7 +130,6 @@ extension TodoListViewController: UISearchBarDelegate {
             }
         }
     }
-}
-
+}    
 
 
